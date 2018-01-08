671c15216a3b6285eb4cac0bb9aebd79c8fe6b0b
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-08 16:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesInstruments = [];
-  for (let i = 0; i < musicians.length)
+  for (let i = 0; i < musicians.length; i++) {
+    beatlesInstruments.push(`)
+  }
 }
